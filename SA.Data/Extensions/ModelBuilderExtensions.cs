6a54ec11305a892f2f55f58c5bf433628a316887
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-02-11 17:19:45 +0300

    added new tables

--- SA.Data/Extensions/ModelBuilderExtensions.cs
+++ SA.Data/Extensions/ModelBuilderExtensions.cs
@@ -2,10 +2,16 @@
 using SA.Domain.Domains.Auth;
 using SA.Domain.Domains.Category;
 using SA.Domain.Domains.Chat;
+using SA.Domain.Domains.Comment;
+using SA.Domain.Domains.Location;
 using SA.Domain.Domains.Media;
+using SA.Domain.Domains.Order;
+using SA.Domain.Domains.Product;
+using SA.Domain.Domains.Saller;
 using SA.Domain.Domains.User;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Metrics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +30,18 @@ namespace SA.Data.Extensions
 			modelBuilder.Entity<ImageEntity>().ToTable("Image");
 			modelBuilder.Entity<UserEntity>().ToTable("User");
 			modelBuilder.Entity<UserDetailEntity>().ToTable("UserDetail");
+			modelBuilder.Entity<CommentEntity>().ToTable("Comment");
+			modelBuilder.Entity<CommentReplyEntity>().ToTable("CommentReply");
+			modelBuilder.Entity<AddressEntity>().ToTable("Address");
+			modelBuilder.Entity<CityEntity>().ToTable("City");
+			modelBuilder.Entity<CountryEntity>().ToTable("Country");
+			modelBuilder.Entity<OrderDetailsEntity>().ToTable("OrderDetails");
+			modelBuilder.Entity<OrderEntity>().ToTable("Order");
+			modelBuilder.Entity<ProductCategoryEntity>().ToTable("ProductCategory");
+			modelBuilder.Entity<ProductEntity>().ToTable("Product");
+			modelBuilder.Entity<SallerProductEntity>().ToTable("SallerProduct");
+			modelBuilder.Entity<SallerEntity>().ToTable("Saller");
+			modelBuilder.Entity<UserRolesEntity>().ToTable("UserRoles");
 		}
 	}
 }
