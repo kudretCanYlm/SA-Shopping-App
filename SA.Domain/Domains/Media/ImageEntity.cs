d9215a311a791afc24bff4f4991834fb2e17badd
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-02-11 17:29:27 +0300

    added news and refactored

--- SA.Domain/Domains/Media/ImageEntity.cs
+++ SA.Domain/Domains/Media/ImageEntity.cs
@@ -1,34 +1,44 @@
 ﻿using SA.Domain.Base;
 using SA.Domain.Base.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SA.Domain.Domains.Media
 {
-	public class ImageEntity : BaseEntity
-	{
-		public ImageEntity():base()
-		{
+    public class ImageEntity : BaseEntity
+    {
+        public ImageEntity() : base()
+        {
 
-		}
+        }
 
-		public string FileName { get; set; }
-		public string FileExtension { get; set; }
-		public byte[] ImageData { get; set; }
+        public string FileExtension { get; set; }
+        public byte[] ImageData { get; set; }
 
-		public ImageTypesEnum ImageType { get; set; }
-		//public bool IsPublic { get; set; }
+        public ImageTypesEnum ImageType { get; set; }
+        public Guid ImageOwnerId { get; set; }
 
 
-		protected override IEnumerable<object> GetEqualityComponents()
-		{
-			// Using a yield return statement to return each element one at a time
-			yield return FileName; 
-			yield return FileExtension; 
-			yield return ImageData;
-		}
-	}
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            // Using a yield return statement to return each element one at a time
+            yield return FileExtension;
+            yield return ImageData;
+        }
+
+        public static string ToBase64Image(ImageEntity imageEntity)
+        {
+            string base64 = "data:" + imageEntity.FileExtension + ";base64," + Convert.ToBase64String(imageEntity.ImageData);
+
+            return base64;
+        }
+
+        public static string ToImageFileExtension(string base64)
+        {
+            return base64.Split(';')[0].Substring(5);
+        }
+
+        public static byte[] ToByteArrayImage(string base64)
+        {
+            return Convert.FromBase64String(base64.Split(',')[1]);
+        }
+    }
 }
