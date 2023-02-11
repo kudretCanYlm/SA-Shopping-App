3bbac8cd7ad04057ff57a39d7564340bb6ab0589
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-02-11 17:25:10 +0300

    first commit

---
+++ SA.Data/Repository/Location/SubCity/ISubCityRepository.cs
@@ -0,0 +1,14 @@
+﻿using SA.Data.Repository.Base;
+using SA.Domain.Domains.Location;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SA.Data.Repository.Location.SubCity
+{
+	public interface ISubCityRepository:IRepository<SubCityEntity>
+	{
+	}
+}
