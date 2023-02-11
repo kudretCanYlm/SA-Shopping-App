bee56d01de93769037db04ee97e22ddc320d4d90
Kudret Can Yalım <69641556+kudretCanYlm@users.noreply.github.com>
2023-02-11 17:28:16 +0300

    added news

--- SA.Domain/Domains/Auth/LoginEntity.cs
+++ SA.Domain/Domains/Auth/LoginEntity.cs
@@ -1,46 +1,72 @@
 ﻿using SA.Domain.Base;
+using SA.Domain.Domains.User;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Security.Cryptography;
 using System.Text;
 
 namespace SA.Domain.Domains.Auth
 {
-	//will add logs for per IP
-	public class LoginEntity : BaseEntity
-	{
-		public LoginEntity():base()
-		{
+    //will add logs for per IP
+    public class LoginEntity : BaseEntity
+    {
+        public LoginEntity() : base()
+        {
 
-		}
+        }
 
-		public string Username { get; set; }
+        public string Username { get; set; }
 
-		public string Password { get; set; }
+        public string Password { get; set; }
 
-		public DateTime? LastLogin { get; set; }
+        public DateTime? LastLogin { get; set; }
 
-		protected override IEnumerable<object> GetEqualityComponents()
-		{
-			yield return Username;
-			yield return Password;
-		}
+        public Guid UserId { get; set; }
+        public RoleEnum Role { get; set; }
 
-		public void ToHashPassword()
-		{
-			// Create a SHA256   
-			using (SHA256 sha256Hash = SHA256.Create())
-			{
-				// ComputeHash - returns byte array  
-				byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(this.Password));
+        [ForeignKey(nameof(UserId))]
+        public UserEntity User { get; set; }
 
-				// Convert byte array to a string   
-				StringBuilder builder = new StringBuilder();
-				for (int i = 0; i < bytes.Length; i++)
-				{
-					builder.Append(bytes[i].ToString("x2"));
-				}
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            yield return Username;
+            yield return Password;
+        }
 
-				this.Password=builder.ToString();
-			}
-		}
-	}
+        public void ToHashPassword()
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(this.Password));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                this.Password = builder.ToString();
+            }
+        }
+        public static string ToHashPassword(string value)
+        {
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(value));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+
+                return builder.ToString();
+            }
+        }
+    }
 }
